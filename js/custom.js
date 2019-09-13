390b95eecc912143294ec23ad7322e76b48d8339
Gerald Olumide <30971557+Lucignation@users.noreply.github.com>
2019-09-13 13:53:50 -0700

    This is the first commit
    More commits will be added

---
+++ js/custom.js
@@ -0,0 +1,140 @@
+/*============ NAVBAR TRANSPARENT TO SOLID ============*/
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 300) {
+            $('.navbar').addClass('solid');
+        }else{
+            $('.navbar').removeClass('solid');
+        }
+    });
+});
+
+
+/*============ CLOSE MOBILE NAV ON CLICK ============*/
+
+$(document).ready(function(){
+    $(document).click(function(event){
+        let clickover = $(event.target);
+        let _opened = $('.navbar-collapse').hasClass('show');
+        if(_opened === true && !clickover.hasClass('navbar-toggler')){
+            $('.navbar-toggler').click();
+        }
+    });
+});
+
+
+/*============ SMOOTH SCROLLING TO LINKS ============*/
+
+$(document).ready(function(){
+    $("a").on('click', function(event){
+        if(this.hash !== ""){
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }//End of the If statement
+    });
+});
+
+
+/*============ BOUNCING DOWN ARROW ============*/
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
+    });
+});
+
+
+/*============ MEET THE TEAM ============*/
+
+$(document).ready(function(){
+    $("#team-slider").owlCarousel({
+        item:3,
+        autoplay:true,
+        smartSpeed:700,
+        loop:true,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            }
+        }
+    }
+    );
+});
+
+
+/*============ SKILLS COUNTER ============*/
+
+$(document).ready(function(){
+    $(".counter").counterUp({
+        delay:10,
+        time:1800
+    });
+});
+
+/*============ CLIENTS CAROUSEL ============*/
+
+$(document).ready(function(){
+    $("#clients-slider").owlCarousel({
+        item:2,
+        autoplay:true,
+        smartSpeed:1700,
+        loop:true,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    }
+    );
+});
+
+/*============ TOP SCROLL ============*/
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 500){
+            $('.top-scroll').fadeIn();
+        }else{
+            $('.top-scroll').fadeOut();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
